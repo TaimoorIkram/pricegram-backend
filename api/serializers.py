b07dd257295bf82abe821715a03e768805074ca9
Affan Bin Faisal <afaisal.bese21seecs@seecs.edu.pk>
2023-04-30 14:31:18 +0500

    fuck yu

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from restbase.models import Person
+from restbase.models import Person, Product
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
         fields = '__all__'
