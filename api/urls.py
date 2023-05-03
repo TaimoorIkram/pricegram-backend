d02b644970132cdbe4a85b7d0b07094f3658a082
Affan Bin Faisal <afaisal.bese21seecs@seecs.edu.pk>
2023-05-03 16:00:25 +0500

    MySQL & Django

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getAllItems),
-    path('add/', views.addPerson),
     path('products/', views.getProducts, name = 'products'),
     path('product/<int:id>/', views.getProduct, name = 'product'),
 ]
--- restbase/models.py
+++ restbase/models.py
@@ -2,16 +2,6 @@ from django.db import models
 
 NULL = "NULL"
 
-# Create your models here.
-class Person(models.Model):
-    fname = models.CharField(null=False, max_length=50)
-    lname = models.CharField(null=False, max_length=50)
-    age = models.IntegerField(null=False, default=0)
-    mail = models.CharField(null=False, max_length=50, default=NULL)
-
-    def __str__(self) -> str:
-        return self.fname + ' ' + self.lname
-
 class Product(models.Model):
     title = models.CharField(max_length=1000)
     original_price = models.IntegerField()
